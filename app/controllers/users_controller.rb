cfbd1ca4fc291f00587c6180f561b3253f015e9d
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-31 15:01:57 +0800

    Add methods to application, users, user_sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :require_login, only: [:new, :create]
 
   def index
     @users = User.order('updated_at DESC')
@@ -15,7 +16,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @users, notice: 'User was successfully created.'
+      auto_login(@user)
+      redirect_to :users, notice: 'User was successfully created.'
     else
       render :new
     end
