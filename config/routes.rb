8ad47ab0b1704c44f8b7c0cfc8e20dd123241f36
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-31 14:48:05 +0800

    Generate user_sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'user_sessions/new'
+
+  get 'user_sessions/crete'
+
+  get 'user_sessions/destroy'
+
   resources :users
   root 'home#index'
   # devise_for :users
