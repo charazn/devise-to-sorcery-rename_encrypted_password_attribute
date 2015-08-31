f89a85386f66a41d1834bb3603eb64ac5bfe4ff1
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-31 12:28:28 +0800

    Add index and protect show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: :show  
   def index
+  end
+
+  def show
+    @users = User.order('updated_at DESC')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
 
+  get '/show' => 'home#show'
+
 end
