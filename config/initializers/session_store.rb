7ab07ffef9305a43d26864ff22ed39e0a188f8b5
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-08-31 11:13:09 +0800

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_devise-to-sorcery-rename_encrypted_password_attribute_session'
